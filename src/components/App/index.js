d7747b41b413821eaab28d487222145fd80afa57
Isabelle <isabellaRebecca@live.co.uk>
2022-11-14 21:02:30 +0000

    completed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,21 +4,26 @@ import "./App.css";
 import PokemonViewer from "../PokemonViewer";
 
 function App() {
-  const [id, setId] = useState();
-
+  const [id, setId] = useState(1);
+  
   function handleClick() {
     // TODO: Set id to be random number between 1 and 151
-    setId(151);
+    let random = Math.floor(Math.random()*151); 
+    setId(random);
+
+
   }
 
   return (
     <div className="App">
       {/* TODO: call handleClick when button clicked */}
-      <button>Get Random Pokemon</button>
+
+      <button onClick = {handleClick}>Get Random Pokemon</button>
       {/* TODO: hand down id as a prop */}
-      <PokemonViewer />
+      <PokemonViewer id = {id} />
     </div>
   );
 }
 
 export default App;
+
--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function PokemonViewer({ id }) {
   // TODO: send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
   // HINT: you will need useState and useEffect!
+  const [pokemon, setPokemon] = useState();
+  
+
+  useEffect(() => {
+    async function getPokemon(){
+      const response  = await fetch (`https://pokeapi.co/api/v2/pokemon/${id}` )
+      const data = await response.json();
+      setPokemon(data);
+      console.log(data);
+    }
+    getPokemon()
+  },[id])
+  
+  console.log(pokemon)
   return (
     <div className="pokemon-viewer">
-      <p>display pokemon with id {id} here!</p>
+    <h1> {pokemon.name} </h1>
+    <p>display pokemon with id {id} here!</p>
     </div>
   );
 }
