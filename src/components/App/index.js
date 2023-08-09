3a5ef51abce290979997d58130b39aa91c03c5da
remiyeku <antakin4@gmail.com>
2023-08-09 22:54:38 +0100

    added sprite functionality to app - forgot to fork

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 import "./App.css";
-
 import PokemonViewer from "../PokemonViewer";
 
 function App() {
   const [id, setId] = useState();
-
+  let random = Math.floor(Math.random() * 152);
   function handleClick() {
-    // TODO: Set id to be random number between 1 and 151
-    setId(151);
+    setId(random);
   }
 
   return (
     <div className="App">
-      {/* TODO: call handleClick when button clicked */}
-      <button>Get Random Pokemon</button>
-      {/* TODO: hand down id as a prop */}
-      <PokemonViewer />
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Get Random Pokemon
+      </button>
+      <PokemonViewer id={random} />
+      <p>Pokedex: {id}</p>
     </div>
   );
 }
--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
+
 function PokemonViewer({ id }) {
-  // TODO: send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
-  // HINT: you will need useState and useEffect!
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    async function getPokemon() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+        headers: { accepts: "application/json" },
+      });
+      const data = await response.json();
+      console.log(data, "this is data");
+      setPokemon(data);
+    }
+    getPokemon(id);
+  }, [id]);
   return (
     <div className="pokemon-viewer">
-      <p>display pokemon with id {id} here!</p>
+      <p>{pokemon.name}</p>
+      <img
+        src={pokemon.sprites?.other.home.front_default}
+        alt="Random pokemon"
+      />
+      <p>{pokemon.weight}lbs</p>
     </div>
   );
 }
